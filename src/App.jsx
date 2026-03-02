95258e26f29aed671aab4d84c32e2526c5188023
Paoo <thienhanaylai@gmail.com>
2026-03-02 14:33:22 +0700

    update meta info, update UX

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { Outlet } from "react-router";
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
